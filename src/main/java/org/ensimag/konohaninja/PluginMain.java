91f2e682baf1346207389b45c11001f2cc88cf5d
elias-bina <elias.bina58@gmail.com>
2021-11-08 20:04:30 +0100

    Project setup

---
+++ src/main/java/org/ensimag/konohaninja/PluginMain.java
@@ -0,0 +1,22 @@
+package org.ensimag.konohaninja;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginMain extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Konoha no Shinobi enabled");
+
+        // this.getCommand("uwu").setExecutor(new CommandBasic());
+        // this.getCommand("sortchest").setExecutor(SortingCommand.getInstance());
+        // this.getCommand("ninjamode").setExecutor(NinjaModeCommand.getInstance());
+        // getServer().getPluginManager().registerEvents(ListenerBasic.getInstance(), this);
+        // getServer().getPluginManager().registerEvents(ChestSortingListener.getInstance(), this);
+        // getServer().getPluginManager().registerEvents(ItemReplacingListener.getInstance(), this);
+        
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("Konoha no Shinobi disabled");
+    }
+}
