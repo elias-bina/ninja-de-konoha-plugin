bb2cb9a73f89f26750986a301be58e06d7dc464c
elias-bina <elias.bina58@gmail.com>
2021-12-09 16:54:56 +0100

    TP to center

--- src/main/java/org/ensimag/konohaninja/jutsu/MulticloneCommand.java
+++ src/main/java/org/ensimag/konohaninja/jutsu/MulticloneCommand.java
@@ -3,14 +3,17 @@ package org.ensimag.konohaninja.jutsu;
 
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.*;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import net.citizensnpcs.api.CitizensAPI;
@@ -18,10 +21,13 @@ import net.citizensnpcs.api.npc.NPC;
 
 public class MulticloneCommand implements CommandExecutor {
 
+    private static final double MULTICLONE_RANGE = 10.0;
+    private static final int CLONE_NUMBER = 9;
+
     private static MulticloneCommand instance;
 
     private final Map<String, NPC> cachedPlayerClone;
-    private final Map<String, List<HumanEntity>> clonesPerPlayer;
+    private final Map<String, List<NPC>> clonesPerPlayer;
 
     private MulticloneCommand() {
         this.clonesPerPlayer = new HashMap<>();
@@ -43,6 +49,9 @@ public class MulticloneCommand implements CommandExecutor {
         if(cachedPlayerClone.get(playerName) == null){
             cachedPlayerClone.put(playerName, CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, playerName));
         }
+        if(clonesPerPlayer.get(playerName) == null){
+            clonesPerPlayer.put(playerName, new LinkedList<>());
+        }
     }
 
     @Override
@@ -53,6 +62,22 @@ public class MulticloneCommand implements CommandExecutor {
         if (sender instanceof Player player) {
 
            NPC e = cachedPlayerClone.get(player.getDisplayName()).copy();
+
+           List<NPC> l = clonesPerPlayer.get(player.getDisplayName());
+
+           Vector direction = player.getLocation().getDirection();
+           direction.setY(0);
+           direction.normalize();
+           direction.multiply(MULTICLONE_RANGE);
+
+           Location center = player.getLocation().add(direction);
+
+           for(int i = 0; i < CLONE_NUMBER;i++){
+
+           }
+
+           l.add(e);
+
            Bukkit.getLogger().info(e.toString());
 
             // SkinTrait skinTrait = new SkinTrait();
